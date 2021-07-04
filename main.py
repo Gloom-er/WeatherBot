b84f0451e219b883bd254c6162f2d23b1d8de42b
Kirill <kuchiki99@mail.ru>
2021-07-04 21:04:58 +0300

    Работающее краткое описание

--- main.py
+++ main.py
@@ -13,12 +13,8 @@ check = requests.get(link_short).text
 
 soup = BeautifulSoup(check, 'lxml')
 
-block_day = soup.find('div', class_="temp forecast-briefly__temp forecast-briefly__temp_day")
-day = block_day.find_all('span')[1].text
-block_night = soup.find('div', class_="temp forecast-briefly__temp forecast-briefly__temp_night")
-night = block_night.find_all('span')[1].text
-status = soup.find('div', class_="forecast-briefly__condition").text
-result = f'Температура днем: {day}\nТемпература ночью: {night}\n{status}'
+block = soup.find('div', class_='forecast-briefly__days')
+test = block.find('ul', class_='swiper-wrapper')
 
 
 @bot.message_handler(commands=['start'])
@@ -81,14 +77,86 @@ def callback(call):
     try:
         if call.message:
             if call.data == 'Yes_Today':
-                bot.send_message(call.message.chat.id, result)
 
+                day = test.find_all('div')[7].text[4:]
+                night = test.find_all('div')[8].text[5:]
+                status = test.find_all('div')[9].text
+
+                result = f'Температура днем: {day}\nТемпература ночью: {night}\n{status}'
+
+                bot.send_message(call.message.chat.id, result)
                 #   remove inline button
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Кратко',
                                       reply_markup=None)
+
+            elif call.data == 'Yes_Tomorrow':
+                day = test.find_all('div')[12].text[4:]
+                night = test.find_all('div')[13].text[5:]
+                status = test.find_all('div')[14].text
+
+                result = f'Температура днем: {day}\nТемпература ночью: {night}\n{status}'
+
+                bot.send_message(call.message.chat.id, result)
+                #   remove inline button
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Кратко',
+                                      reply_markup=None)
+
+            elif call.data == 'Yes_Week':
+
+                day = test.find_all('div')[7].text[4:]
+                night = test.find_all('div')[8].text[5:]
+                status = test.find_all('div')[9].text
+
+                day1 = test.find_all('div')[12].text[4:]
+                night1 = test.find_all('div')[13].text[5:]
+                status1 = test.find_all('div')[14].text
+
+                name2 = test.find_all('div')[16].text
+                day2 = test.find_all('div')[17].text[4:]
+                night2 = test.find_all('div')[18].text[5:]
+                status2 = test.find_all('div')[19].text
+
+                name3 = test.find_all('div')[21].text
+                day3 = test.find_all('div')[22].text[4:]
+                night3 = test.find_all('div')[23].text[5:]
+                status3 = test.find_all('div')[24].text
+
+                name4 = test.find_all('div')[26].text
+                day4 = test.find_all('div')[27].text[4:]
+                night4 = test.find_all('div')[28].text[5:]
+                status4 = test.find_all('div')[29].text
+
+                name5 = test.find_all('div')[31].text
+                day5 = test.find_all('div')[32].text[4:]
+                night5 = test.find_all('div')[33].text[5:]
+                status5 = test.find_all('div')[34].text
+
+                name6 = test.find_all('div')[36].text
+                day6 = test.find_all('div')[37].text[4:]
+                night6 = test.find_all('div')[38].text[5:]
+                status6 = test.find_all('div')[39].text
+
+                result = f'Сегодня:\nПогода днем:{day}\nПогода вечером:{night}\n{status}\n' \
+                         f'Завтра:\nПогода днем:{day1}\nПогода вечером:{night1}\n{status1}\n' \
+                         f'{name2}:\nПогода днем:{day2}\nПогода вечером:{night2}\n{status2}\n' \
+                         f'{name3}:\nПогода днем:{day3}\nПогода вечером:{night3}\n{status3}\n' \
+                         f'{name4}:\nПогода днем:{day4}\nПогода вечером:{night4}\n{status4}\n' \
+                         f'{name5}:\nПогода днем:{day5}\nПогода вечером:{night5}\n{status5}\n' \
+                         f'{name6}:\nПогода днем:{day6}\nПогода вечером:{night6}\n{status6}\n'
+
+                bot.send_message(call.message.chat.id, result)
+                #   remove inline button
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Кратко',
+                                      reply_markup=None)
+
             elif call.data == 'No_Today':
                 bot.send_message(call.message.chat.id, 'Кек1')
+                #   remove inline button
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Подробно',
+                                      reply_markup=None)
 
+            elif call.data == 'No_Tomorrow':
+                bot.send_message(call.message.chat.id, 'Кек2')
                 #   remove inline button
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Подробно',
                                       reply_markup=None)
